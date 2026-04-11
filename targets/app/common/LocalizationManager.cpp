2396e6bb9663e1e5b6981fa3d8598e35e6d69747
Tropical <42101043+tropicaaal@users.noreply.github.com>
2026-04-11 00:17:49 -0500

    fix: pulled a 4J

--- targets/app/common/LocalizationManager.cpp
+++ targets/app/common/LocalizationManager.cpp
@@ -21,7 +21,7 @@
 #include "platform/renderer/renderer.h"
 #include "strings.h"
 #include "util/StringHelpers.h"
-#include "app/common/ui/ConsoleUIController.h"
+#include "app/common/UI/ConsoleUIController.h"
 
 int LocalizationManager::s_iHTMLFontSizesA[eHTMLSize_COUNT] = {20, 13, 20, 26};
 
