d74c236de914956db44a38bd7dfb24ff85849299
FloofyPlasma <github@floofyplasma.com>
2026-04-12 14:32:34 -0400

    fix(macos): document SDL2 invisible cursor patch

--- targets/platform/input/sdl2/SDL2CursorPatch.m
+++ targets/platform/input/sdl2/SDL2CursorPatch.m
@@ -1,6 +1,11 @@
 #import <AppKit/AppKit.h>
 #import <objc/runtime.h>
 
+// Fixes a bug with SDL2 on macOS versions newer than 14.
+// Slightly tweaked patch from this issue.
+// resetCursorRects may in the future need patched.
+// https://github.com/libsdl-org/SDL/issues/9745
+// This has been fixed in SDL3 I believe.
 void PatchSDLInvisibleCursor(void) {
     Class cls = objc_getMetaClass("NSCursor");
     if (!cls) return;
@@ -8,8 +13,8 @@ void PatchSDLInvisibleCursor(void) {
     SEL sel = sel_registerName("invisibleCursor");
 
     IMP replacement = imp_implementationWithBlock(^NSCursor*(id self) {
-        NSImage* img = [[NSImage alloc] initWithSize:NSMakeSize(1, 1)];
-        return [[NSCursor alloc] initWithImage:img hotSpot:NSZeroPoint];
+      NSImage* img = [[NSImage alloc] initWithSize:NSMakeSize(1, 1)];
+      return [[NSCursor alloc] initWithImage:img hotSpot:NSZeroPoint];
     });
 
     Method m = class_getClassMethod(objc_getClass("NSCursor"), sel);
